e1efb83174c9fea4b19f1e8dd057ee781f6cc607
Alexandre Turbillon <aturbillon@nexdom.adds>
2017-04-28 17:45:54 +0200

    adding a data faker to generate data for kafka

--- src/main/java/com/serialiser/Main.java
+++ src/main/java/com/serialiser/Main.java
@@ -3,6 +3,7 @@ package com.serialiser;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +18,19 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
+
         final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
+        Faker faker = new Faker();
+        for(int i=0;i<100;i++) {
+            String name = faker.name().fullName();
+            String streetAddress = faker.address().streetAddress();
+            String emailAddress = faker.internet().emailAddress();
+
+            System.out.println(name + "; " + streetAddress + "; " + emailAddress+"; ");
+        }
+
 
         ObjectMapper mapper = new ObjectMapper();
         Ticket t = null;
